4db58270d9e5031df8d606b265039e60e71cd3f2
adityarayapati <adityaram1987@gmail.com>
2024-05-09 22:30:05 +0530

    initial commit

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+
+
+//Todo : Paste the credentials from your project console in back4app.com
+const kParseApplicationId = "4Gg2Q11cyvyG4HpUjICh9LhqWRGr7Fo8i6yzpN7s";
+const kParseRestApiKey = "Xio7C83qIXmkJYudRJlx5tQ7N21fMCGyRRfaecPE";
